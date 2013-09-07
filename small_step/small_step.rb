aba6e5fae548602164706100b602f4eed561e33f
Przemek Owczarek <przemek@futuresimple.com>
2013-09-07 23:27:33 +0200

    Extractione

--- small_step/small_step.rb
+++ small_step/small_step.rb
@@ -1,4 +1,27 @@
+module Inspectable
+  def inspect
+    "<#{self}>"
+  end
+end
+
+module BinaryOp
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      self.class.new(left.reduce, right)
+    elsif right.reducible?
+      self.class.new(left, right.reduce)
+    else
+      Number.new(operator(left.value, right.value))
+    end
+  end
+end
+
 class Number < Struct.new(:value)
+  include Inspectable
 
   def reducible?
     false
@@ -8,58 +31,33 @@ class Number < Struct.new(:value)
     value.to_s
   end
 
-  def inspect
-    "<#{self}>"
-  end
 end
 
 class Add < Struct.new(:left, :right)
-
-  def reducible?
-    true
-  end
-
-  def reduce
-    if left.reducible?
-      Add.new(left.reduce, right)
-    elsif right.reducible?
-      Add.new(left, right.reduce)
-    else
-      Number.new(left.value + right.value)
-    end
-  end
+  include Inspectable
+  include BinaryOp
 
   def to_s
     "#{left} + #{right}"
   end
 
-  def inspect
-    "<#{self}>"
+  private
+  def operator(left, right)
+    left + right
   end
 
 end
 
 class Multiply < Struct.new(:left, :right)
-
-  def reducible?
-    true
-  end
-
-  def reduce
-    if left.reducible?
-      Multiply.new(left.reduce, right)
-    elsif right.reducible?
-      Multiply.new(left, right.reduce)
-    else
-      Number.new(left.value * right.value)
-    end
-  end
+  include Inspectable
+  include BinaryOp
 
   def to_s
     "#{left} * #{right}"
   end
 
-  def inspect
-    "<#{self}>"
+  private
+  def operator(left, right)
+    left * right
   end
 end
