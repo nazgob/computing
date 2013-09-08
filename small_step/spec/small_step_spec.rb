ca1399133361a26603d4e25e0eb4b7bc49edf4bb
Przemek Owczarek <przemek@futuresimple.com>
2013-09-08 15:27:38 +0200

    Spec reduce better + move LessThen spec to proper place

--- small_step/spec/small_step_spec.rb
+++ small_step/spec/small_step_spec.rb
@@ -36,11 +36,15 @@ describe 'SmallStep' do
       its(:reduce) { should eq(six) }
 
       it '#reduce left' do
-        expect(Add.new(subject, seven).reduce.to_s).to eq('6 + 7')
+        operation = Add.new(subject, seven)
+        expect(operation.reduce.to_s).to eq('6 + 7')
+        expect(operation.reduce.reduce.to_s).to eq('13')
       end
 
       it '#reduce right' do
-        expect(Add.new(seven, subject).reduce.to_s).to eq('7 + 6')
+        operation = Add.new(seven, subject)
+        expect(operation.reduce.to_s).to eq('7 + 6')
+        expect(operation.reduce.reduce.to_s).to eq('13')
       end
     end
 
@@ -53,33 +57,36 @@ describe 'SmallStep' do
       its(:reduce) { should eq(eight) }
 
       it '#reduce left' do
-        expect(Multiply.new(subject, seven).reduce.to_s).to eq('8 * 7')
+        operation = Multiply.new(subject, seven)
+        expect(operation.reduce.to_s).to eq('8 * 7')
+        expect(operation.reduce.reduce.to_s).to eq('56')
       end
 
       it '#reduce right' do
         expect(Multiply.new(seven, subject).reduce.to_s).to eq('7 * 8')
       end
 
-      describe LessThen do
-        subject { LessThen.new(two, four) }
-        let(:reducible_operation) { Multiply.new(two, four) }
+    end
 
-        its(:to_s) { should eq('2 < 4') }
-        its(:inspect) { should eq('<2 < 4>') }
-        its(:reducible?) { should be_true }
-        its(:reduce) { should eq(yes) }
+    describe LessThen do
+      subject { LessThen.new(two, four) }
 
-        it '#reduce left' do
-          operation = LessThen.new(reducible_operation, seven)
-          expect(operation.reduce.to_s).to eq('8 < 7')
-          expect(operation.reduce.reduce.to_s).to eq('false')
-        end
+      let(:reducible_operation) { Multiply.new(two, four) }
+      its(:to_s) { should eq('2 < 4') }
+      its(:inspect) { should eq('<2 < 4>') }
+      its(:reducible?) { should be_true }
+      its(:reduce) { should eq(yes) }
 
-        it '#reduce right' do
-          operation = LessThen.new(seven, reducible_operation)
-          expect(operation.reduce.to_s).to eq('7 < 8')
-          expect(operation.reduce.reduce.to_s).to eq('true')
-        end
+      it '#reduce left' do
+        operation = LessThen.new(reducible_operation, seven)
+        expect(operation.reduce.to_s).to eq('8 < 7')
+        expect(operation.reduce.reduce.to_s).to eq('false')
+      end
+
+      it '#reduce right' do
+        operation = LessThen.new(seven, reducible_operation)
+        expect(operation.reduce.to_s).to eq('7 < 8')
+        expect(operation.reduce.reduce.to_s).to eq('true')
       end
     end
   end
