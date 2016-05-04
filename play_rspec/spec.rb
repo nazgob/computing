185ffb2339167bcf59732ddf043f2badeffd637c
Przemek Owczarek <nazgob@gmail.com>
2016-05-04 22:35:25 +0200

    Playing with specs

--- play_rspec/spec.rb
+++ play_rspec/spec.rb
@@ -1,3 +1,22 @@
-def foo
-  42
+module Bazz
+  def buzz
+    'buzz'.freeze
+  end
+
+  def bar
+    'bar'.freeze
+  end
+
+  def buzzbar
+    buzz + bar
+  end
 end
+
+class Foo
+  include Bazz
+
+  def foo
+    'foo'.freeze
+  end
+end
+
--- play_rspec/spec/meta_spec.rb
+++ play_rspec/spec/meta_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 require_relative '../spec.rb'
 
-describe 'stuff' do
+describe Foo do
+  let(:foo) { Foo.new }
+
   it 'works' do
-    expect(foo).to eq(42)
+    expect(subject.buzz).to eq('buzz')
+  end
+
+  it 'buzzes barr' do
+    expect(subject.buzzbar).to eq('buzzbar')
+  end
+
+  it 'stubs instance method' do
+    allow(subject).to receive(:foo).and_return(42)
+    expect(subject.foo).to eq(42)
+  end
+
+  it 'stubs module method' do
+    allow(subject).to receive(:bar).and_return('wat')
+    expect(subject.buzzbar).to eq('buzzwat')
   end
 end
 
